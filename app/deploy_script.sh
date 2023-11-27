cd1fed21ae37338a6e142ab54ce5b264e51eb121
PC1 <alexandre.dupuis9@gmail.com>
2023-11-27 18:07:25 +0100

    fix test path

--- app/deploy_script.sh
+++ app/deploy_script.sh
@@ -4,5 +4,5 @@
 # Installez les dépendances depuis le fichier requirements.txt
 pip3 install -r /home/ubuntu/app/requirements.txt
 
-python3 api.py
-python3 front.py
+python3 /home/ubuntu/app/api.py &
+python3 /home/ubuntu/app/front.py &
